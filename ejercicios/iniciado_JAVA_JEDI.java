48f31df45cfdba1ea648a98a7a18e47d8707a9b9
Supernova94oficial <supernova94.666@gmail.com>
2024-07-25 19:57:55 -0300

    commit de prueba :b

--- ejercicios/iniciado_JAVA_JEDI.java
+++ ejercicios/iniciado_JAVA_JEDI.java
@@ -1,1 +1,26 @@
 // suma de dos numeros
+
+//////////////////////////////////////////////////////////////////////////////
+
+/*
+Dado un arreglo de 4 posiciones crear un programa en java
+que permita tomar los dos valores del medio eliminando los extremos.
+Mostrar por pantalla el resultado
+
+{1,2,3,4} --> {2,3}
+
+ */
+
+public class iniciado_JAVA_JEDI {
+    public static void main(String[] args){
+
+        int[] enteros = {1,2,3,4};
+
+        for(int i=enteros.length-2; i>0; i--){
+            System.out.println("indice "+i+ " valor "+enteros[i]);
+        }
+    }
+
+} 
+
+/////////////////////////////////////////////////////////////////////////////
